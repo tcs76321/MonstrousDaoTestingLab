614821e687eed2beef955225faa9feedc2781cd1
Trevor Stahl <trevorclarkstahl@gmail.com>
2022-02-23 13:22:05 -0700

    getting the hang of Junit testing now

--- MonstrousDaoTestingLab/src/test/java/com/sg/testing/dao/implementations/MonsterDaoTest.java
+++ MonstrousDaoTestingLab/src/test/java/com/sg/testing/dao/implementations/MonsterDaoTest.java
@@ -4,6 +4,7 @@ import com.sg.testing.model.Monster;
 import com.sg.testing.dao.implementations.AGoodMonsterDao;
 import org.junit.jupiter.api.Test;
 
+import static com.sg.testing.model.MonsterType.YETI;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MonsterDaoTest {
@@ -25,10 +26,21 @@ public class MonsterDaoTest {
 
     @Test
     void addMonsterTest() {
-        Monster aMon = new Monster();
-        Monster resMon = dao.addMonster(123, aMon);
+        Monster aMon = new Monster("Bob", YETI, 0, "pizza");
         try {
-            assertNotNull(resMon);
+            dao.addMonster(123, aMon);
+        } catch (Exception e) {
+            fail("something went wrong");
+        }
+    }
+
+    @Test
+    void getMonsterTest() {
+        Monster aMon = new Monster("Sam", YETI, 0, "deep dish za");
+        dao.addMonster(234, aMon);
+        try {
+            Monster shouldBeAMon = dao.getMonster(234);
+            assertNotNull(shouldBeAMon);
         } catch (Exception e) {
             fail("something went wrong");
         }
